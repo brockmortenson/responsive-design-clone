83525a8f7e513753a0b7f790502a664cf07f2b81
Brock Mortenson <mortensonbrock@gmail.com>
2021-03-17 15:41:59 -0600

    test commit

---
+++ responsive-design-clone/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import { Component } from 'react';
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+
+    }
+  }
+
+  render(){
+    return(
+      <div className='App'>
+        
+      </div>
+    )
+  }
+
+}
+
+export default App;
